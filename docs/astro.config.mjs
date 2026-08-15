07678855a4f9f0edfb6ac159b47ccfa3426b7714
Jan T. Sott <git@idleberg.com>
2026-08-15 15:04:58 +0200

    chore: update sidebar

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -19,7 +19,7 @@ export default defineConfig({
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/idleberg/ardent' }],
 			sidebar: [
 				{ label: 'Getting started', slug: 'getting-started' },
-				{ label: 'Command Line', slug: 'cli-usage' },
+				{ label: 'CLI Usage', slug: 'cli-usage' },
 				{ label: 'Integrations', slug: 'integrations' },
 				{ label: 'Playground', slug: 'playground' },
 			],
